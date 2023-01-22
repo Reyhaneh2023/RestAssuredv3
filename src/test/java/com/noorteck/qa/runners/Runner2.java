a59f93518768441ce4f22ee329a475ef7b558f94
Reyhana.Hussaini <Reyhanehg.hussaini@gmail.com>
2023-01-22 10:36:05 -0500

    dfsdfbb

---
+++ src/test/java/com/noorteck/qa/runners/Runner2.java
@@ -0,0 +1,22 @@
+package com.noorteck.qa.runners;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+					features = "src/test/resources/hr/",
+					glue = "com.noorteck.qa.steps",
+					tags = "@CreateEmpNew",
+					
+							plugin = {"summary","pretty", "html:target/cucumber-reports.html",
+									"json:target/destinations/cucumber-reports.json",					
+									"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+					monochrome = true
+						)
+
+public class Runner2 {
+	
+
+}
